edb59db367f8bae3f580c45555a86e735cecd4b9
shinis(home) <actionshin@gmail.com>
2024-10-22 22:45:22 +0900

    시큐리티 프로젝트 초기화

---
+++ src/main/java/kr/co/cofile/sbbsecurity/SbbSecurityApplication.java
@@ -0,0 +1,13 @@
+package kr.co.cofile.sbbsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbbSecurityApplication.class, args);
+    }
+
+}
